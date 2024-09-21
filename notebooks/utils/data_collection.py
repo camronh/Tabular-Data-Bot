abc67583d5c4a1939f9ddcb549ef44392f68fa11
Camron Haider <camronhaider@gmail.com>
2024-09-21 18:29:38 -0400

    v0.1

---
+++ notebooks/utils/data_collection.py
@@ -0,0 +1,108 @@
+# Assuming you're using a chroma db client for semantic search
+from chromadb import Client
+import openai
+import pandas as pd
+from langchain_core.tools import tool, BaseTool
+import numpy as np
+from sklearn.metrics.pairwise import cosine_similarity
+from pydantic.v1 import BaseModel, Field
+from typing import Optional, Type
+import json
+
+client = openai.OpenAI()
+
+chroma_client = Client()
+
+embeddings_model = "text-embedding-3-large"
+
+
+class MovieSearch(BaseModel):
+    query: str = Field(description="A semantic search for movie data")
+
+
+def get_embeddings(inputs: list[str], model_name: str = embeddings_model):
+    response = client.embeddings.create(
+        input=inputs,
+        model=model_name
+    )
+    return [embedding.embedding for embedding in response.data]
+
+
+def load_df(file_path: str = '../raw/top_50000.pkl') -> pd.DataFrame:
+    print("Loading df")
+    return pd.read_pickle(file_path)
+
+
+def df_to_llm(df: pd.DataFrame) -> list[dict]:
+    """Parse out only what is relevant to the LLM"""
+    movies = []
+    for index, row in df.iterrows():
+        movies.append({
+            "title": row["title"],
+            "status": row["status"],
+            "release_date": row["release_date"],
+            "revenue": row["revenue"],
+            "runtime": row["runtime"],
+            "budget": row["budget"],
+            "url": f"https://www.imdb.com/title/{row['imdb_id']}",
+            "original_language": row["original_language"],
+            "original_title": row["original_title"],
+            "overview": row["overview"],
+            "popularity": row["popularity"],
+            "tagline": row["tagline"],
+            "genres": row["genres"],
+            "production_companies": row["production_companies"],
+            "production_countries": row["production_countries"],
+            "spoken_languages": row["spoken_languages"],
+            "cast": row["cast"],
+            "director": row["director"],
+            "writers": row["writers"],
+            "producers": row["producers"],
+            "rating": row["imdb_rating"],
+            "votes": row["imdb_votes"],
+        })
+    return json.dumps(movies)
+
+
+class MovieSearchTool(BaseTool):
+    name: str = "movie_data_search"
+    description: str = "Use this tool to search for movies using natural language"
+    args_schema: Type[BaseModel] = MovieSearch
+
+    # Define it as part of the class attribute, not the Pydantic model fields
+    df: pd.DataFrame = None
+
+    def __init__(self):
+        super().__init__()  # Ensure you call the parent class' initializer if needed
+        self.df = load_df("../raw/top_50000.pkl")  # Load the dataframe here
+
+    def semantic_search(self, query: str, k: int = 10):
+        # Assuming you already have a model to generate embeddings for the query
+        response = client.embeddings.create(
+            input=query,
+            model=embeddings_model
+        )
+        print("Got query embeddings")
+        query_embedding = np.array(response.data[0].embedding)
+
+        # Precompute the norm of the query embedding
+        query_norm = np.linalg.norm(query_embedding)
+
+        # Compute dot products and cosine similarities
+        self.df['dot_product'] = self.df['embedding'].apply(
+            lambda x: np.dot(query_embedding, x))
+        self.df['similarity'] = self.df['dot_product'] / \
+            (self.df['embedding_norm'] * query_norm)
+
+        # Sort by similarity and get top k results
+        results_df = self.df.sort_values(by='similarity', ascending=False)
+        results_df = results_df.head(k)
+
+        # Sort by vote count
+        results_df = results_df.sort_values(by='vote_count', ascending=False)
+
+        # Return top k results
+        return results_df
+
+    def _run(self, query: str):
+        return df_to_llm(self.semantic_search(query))
---
+++ notebooks/utils/langgraph.py
@@ -0,0 +1,73 @@
+from typing import Annotated, Sequence, TypedDict
+from utils.data_collection import MovieSearchTool
+
+from langchain_core.messages import BaseMessage, AIMessage, ToolMessage, SystemMessage
+
+from langgraph.graph.message import add_messages
+from langgraph.prebuilt import ToolExecutor, ToolInvocation, ToolNode, tools_condition
+from langgraph.graph import END, StateGraph, START
+
+from langchain_openai import ChatOpenAI
+
+
+class AgentState(TypedDict):
+    output: str
+    messages: Annotated[Sequence[BaseMessage], add_messages]
+    documents: list[dict]
+
+
+system_message = SystemMessage(
+    content="You are a helpful assistant that can answer questions about movies.")
+
+
+class Nodes:
+    def __init__(self, tools):
+        self.tools = tools
+        self.tool_executor = ToolExecutor(tools)
+
+    def call_model(self, state: AgentState):
+        llm = ChatOpenAI(
+            model="gpt-4o-mini").bind_tools(self.tools)
+
+        response = llm.invoke([system_message] + state["messages"])
+
+        if not response.tool_calls:
+            return {"output": response.content, "messages": [response]}
+
+        return {"messages": [response]}
+
+
+def make_graph(tools):
+
+    nodes = Nodes(tools)
+    tools_node = ToolNode(tools)
+
+    # Define a new graph
+    workflow = StateGraph(AgentState)
+
+    # Define the nodes we will cycle between
+    workflow.add_node("Agent", nodes.call_model)  # agent
+    workflow.add_node("tools", tools_node)  # retrieval
+
+    # Decide whether to retrieve
+    workflow.add_conditional_edges(
+        "Agent", tools_condition
+    )
+
+    workflow.add_edge("tools", "Agent")
+
+    workflow.set_entry_point("Agent")
+
+    # Compile
+    graph = workflow.compile()
+
+    return graph
+
+
+def create_agent():
+
+    movies_tool = MovieSearchTool()
+
+    graph = make_graph([movies_tool])
+
+    return graph
